ea8c6ad0bf61b58845e02848f532c202f6651648
sebastianmatt <smatt@middlebury.edu>
2014-01-22 11:03:34 -0500

    Update insertUser.php

--- insertUser.php
+++ insertUser.php
@@ -7,8 +7,11 @@
     define('DB_DATABASE', 'wschaaf_Calendar');
     
     $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die ("could not connect");
-    
-    $sql="INSERT INTO Users (email, password, name, uid) VALUES ('$_POST[email]','$_POST[pw]','$_POST[name]', '$_POST[uid]')";
+    $email = htmlspecialchars($_POST[email]);
+    $pw = htmlspecialchars($_POST[pw]);
+    $name = htmlspecialchars($_POST[name]);
+    $uid = htmlspecialchars($_POST[uid]);
+    $sql="INSERT INTO Users (email, password, name, uid) VALUES ('$email','$pw','$name', '$uid')";
     
     if (!mysqli_query($con, $sql)){        
 		die('Error: ' . mysqli_error($con));
