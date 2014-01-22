b05abee2fce05abc9f6577728da044a500d44c17
Greg Krathwohl <gkrathwohl@middlebury.edu>
2014-01-22 11:30:06 -0500

    uses prepared sql statements for create org

--- insertOrg.php
+++ insertOrg.php
@@ -7,24 +7,50 @@ if ($_SESSION[User] == null){
 	echo "Must be logged in to create an orgasmization";
 }
 else{
+
 	//set up the connection to the database
 	define('DB_SERVER', 'panther.cs.middlebury.edu');
 	define('DB_USERNAME', 'wschaaf');
 	define('DB_PASSWORD', 'wschaaf');
 	define('DB_DATABASE', 'wschaaf_Calendar');
 
-	$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die ("could not connect");
+	$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-	$sql="INSERT INTO Organizations (name, description) VALUES ('$_POST[name]','$_POST[description]')";
+	$stmt = $mysqli->prepare("INSERT INTO Organizations (name, description) VALUES (?,?)");
 
-	$sql2="INSERT INTO BelongsTo (orgName, uid) VALUES ('$_POST[name]','$_SESSION[uid]')";
-
-	if (!mysqli_query($con, $sql)){
-		die('Error: ' . mysqli_error($con));
+	if (!$stmt){
+		echo "Prepare failed: (".$mysqli->errno . ") ".$mysqli->error;
 	}
-	if (!mysqli_query($con, $sql2)){
-		die('Error: ' . mysqli_error($con));
+	$name = $_POST['name'];
+
+	$description = $_POST['description'];
+
+
+	if (!$stmt->bind_param("ss",$name,$description)){
+		echo "Binding parameters failed: (" . $stmt->errno . ")".$stmt->error;
 	}
+	if (!$stmt->execute()){
+		echo "Execute failed: (" . $stmt->errno . ")".$stmt->error;
+	}
+
+	$stmt2 = $mysqli->prepare("INSERT INTO BelongsTo (orgName, uid) VALUES (?,?)");
+
+	if (!$stmt2){
+		echo "Prepare failed: (".$mysqli->errno . ") ".$mysqli->error;
+	}
+	
+
+	$uid = $_SESSION['uid'];
+
+
+	if (!$stmt2->bind_param("ss",$name,$uid)){
+		echo "Binding parameters failed: (" . $stmt2->errno . ")".$stmt2->error;
+	}
+
+	if (!$stmt2->execute()){
+		echo "Execute failed: (" . $stmt2->errno . ")".$stmt2->error;
+	}
+
 	echo "Added organization " .$_POST[name];
 	
 }
