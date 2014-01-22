ca0d2837d90e4239ddb2c3f0ff612502cf811c50
sebastianmatt <smatt@middlebury.edu>
2014-01-22 11:31:58 -0500

    Update insertEvent.php

--- insertEvent.php
+++ insertEvent.php
@@ -2,25 +2,32 @@
 
 <!DOCTYPE HTML>
 <?php 
-
-//set up the connection to the database
-define('DB_SERVER', 'panther.cs.middlebury.edu');
-define('DB_USERNAME', 'wschaaf');
-define('DB_PASSWORD', 'wschaaf');
-define('DB_DATABASE', 'wschaaf_Calendar');
-
-$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die ("could not connect");
-
-$sql="INSERT INTO Events (name, time, description, building, room, uid, genre, date, orgName) VALUES ('$_POST[name]','$_POST[time]','$_POST[description]', '$_POST[building]', '$_POST[room]', '$_POST[uid]', '$_POST[genre]', '$_POST[date]', '$_POST[orgName]')";
-
-if (!mysqli_query($con, $sql)){
-    die('Error: ' . mysqli_error($con));
-}
-echo "Added event " .$_POST[name];
+    
+    //set up the connection to the database
+    define('DB_SERVER', 'panther.cs.middlebury.edu');
+    define('DB_USERNAME', 'wschaaf');
+    define('DB_PASSWORD', 'wschaaf');
+    define('DB_DATABASE', 'wschaaf_Calendar');
+    
+    $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die ("could not connect");
+    $name = htmlspecialchars($_POST[name]);
+    $time = htmlspecialchars($_POST[time]);
+    $description = htmlspecialchars($_POST[description]);
+    $building = htmlspecialchars($_POST[building]);
+    $room = htmlspecialchars($_POST[room]);
+    $uid = htmlspecialchars($_POST[uid]);
+    $genre = htmlspecialchars($_POST[genre]);
+    $date = htmlspecialchars($_POST[date]);
+    $orgName = htmlspecialchars($_POST[orgName]);
+    $sql="INSERT INTO Events (name, time, description, building, room, uid, genre, date, orgName) VALUES ('$name','$time','$description', '$building', '$room', '$uid', '$genre', '$date', '$orgName')";
+    
+    if (!mysqli_query($con, $sql)){
+        die('Error: ' . mysqli_error($con));
+    }
+    echo "You created " .$name; 
 ?>
 
 <html>
-</br>
-<a href='./events'>Back to events</a>
-
+	</br>
+	<a href='./events'>Back to events</a>
 </html>
