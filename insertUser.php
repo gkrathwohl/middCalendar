86b8bfaf3343f90d2e69c02a89781e28c4080ac5
sebastianmatt <smatt@middlebury.edu>
2014-01-23 11:47:50 -0500

    Update insertUser.php

--- insertUser.php
+++ insertUser.php
@@ -1,36 +1,59 @@
-<?php 
-	session_start();    
+<?php
+	session_start();
     //set up the connection to the database
     define('DB_SERVER', 'panther.cs.middlebury.edu');
     define('DB_USERNAME', 'wschaaf');
     define('DB_PASSWORD', 'wschaaf');
     define('DB_DATABASE', 'wschaaf_Calendar');
     
+	function encrypt_decrypt($action, $string) {
+		$output = false;
+
+		$encrypt_method = "AES-256-CBC";
+		$secret_key = 'This is my secret key';
+		$secret_iv = 'This is my secret iv';
+
+		// hash
+		$key = hash('sha256', $secret_key);
+		
+		// iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+		$iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+		if( $action == 'encrypt' ) {
+		    $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+		    $output = base64_encode($output);
+		}
+		else if( $action == 'decrypt' ){
+		    $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+		}
+
+		return $output;
+	}
+
     $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die ("could not connect");
     $email = htmlspecialchars($_POST[email]);
     $pw = htmlspecialchars($_POST[pw]);
+	$encryptpw = encrypt_decrypt('encrypt', $pw);
     $name = htmlspecialchars($_POST[name]);
     $uid = htmlspecialchars($_POST[uid]);
-    $sql="INSERT INTO Users (email, password, name, uid) VALUES ('$email','$pw','$name', '$uid')";
-    
-    if (!mysqli_query($con, $sql)){        
-		die('Error: ' . mysqli_error($con));
+    $sql="INSERT INTO Users (email, password, name, uid) VALUES ('$email','$encryptpw','$name', '$uid')";    
+    if (!mysqli_query($con, $sql)){
+	    die('Error: ' . mysqli_error($con));
     }
-	else {
-		Header("Location: ./events.php");		
-		$_SESSION['User'] = $_POST[name];	
-	}
+    else {
+        Header("Location: ./events.php");                
+        $_SESSION['User'] = $_POST[name];        
+    }
 ?>
 
 <!DOCTYPE HTML>
-
-
 <html>
 <head>
 </head>
 
 <body>
-	</br>
-	<a href='./events'>Back to events</a>
+        </br>
+        <a href='./events'>Back to events</a>
 </body>
 </html>
+
