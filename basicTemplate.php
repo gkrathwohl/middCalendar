496b87f1f775edeaf71c4fa0c92140ad14cee692
sebastianmatt <smatt@middlebury.edu>
2014-01-23 11:50:03 -0500

    Create basicTemplate.php

---
+++ basicTemplate.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			body {
+				background-color: gray;
+			}
+			.center {
+				color:black;
+				text-align:center;
+			}
+			.box {
+				background-color: white;
+				width: 60%;
+				padding-top: 5%;
+				padding-bottom: 5%;
+				padding-right: 10%;
+				padding-left: 10%;
+				border-width: 5px;
+				border-style: ridge;
+				border-color: blue;
+				margin-top: 5%;
+				margin-bottom: 5%;
+				margin-right: 10%;
+				margin-left: 10%;
+			}
+			p {
+				font-family:"Times New Roman";
+				font-size:20px;
+			}
+		</style>
+	</head>
+	
+	<body background=wood_background.png>
+		<div class="box"> 
+			<p>Hello world let's make a sick calendar!!!</p>
+		</div>	
+	</body>
+</html>
