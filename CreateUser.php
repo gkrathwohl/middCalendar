9faa4606de29a4162ea65087735243260c61eae4
sebastianmatt <smatt@middlebury.edu>
2014-01-28 23:42:58 -0500

    Update CreateUser.php

--- CreateUser.php
+++ CreateUser.php
@@ -1,99 +1,215 @@
 <?php session_start(); ?>
+<!DOCTYPE html>
 <html>
-<head>
-	<title> Create User </title>
-	<style>
-	#content {
-	  width: 500px ;
-	  margin-left: auto ;
-	  margin-right: auto ;
-	}
-	#content1 {
-	  width: 400px ;
-	  margin-left: auto ;
-	  margin-right: auto ;
-	}
-	</style>
-</head>
+        <head>
+                <style>
+                
+                        
+                        .logo {
+                                position: fixed;
+                                margin-top: 1%;
+                                width: 24%;
+                                height: 15%;
+                                margin-left: 38%;
+                                z-index: 2;
+                                opacity: .6;
+                        }
+                        .header {                                
 
-<body>
-	<div id='content'>
-		<br>
-		
-		Create an Account:
-		Please use a different password from your Middlebury account
-	</div>
-	<br> <br>
-	<div id='content1'>
-		<SCRIPT LANGUAGE="javascript">
-			var goodColor = "#66cc66";
-			var badColor = "#ff6666";
-			function checkEmail() {
-				var email = document.getElementById('email');
-				var message = document.getElementById('emailConfirm');
-				var emailRegex = /\S+@\S+\.\S+/;
-				if(emailRegex.test(email.value)) {
-					email.style.backgroundColor = "#ffffff";
-					message.style.color = goodColor;
-					message.innerHTML = "Valid";
-					submitButton.type = "submit";
-				}
-				else {
-					email.style.backgroundColor = badColor;
-					message.style.color = badColor;
-					message.innerHTML = "Not valid email!";
-					submitButton.type = "hidden";
-				}
-			}		
-			function checkPass() {
-				//Store the password field objects into variables ...
-				var pass1 = document.getElementById('pass1');
-				var pass2 = document.getElementById('pass2');
-				//Store the Confimation Message Object ...
-				var message = document.getElementById('passwordConfirm');
-				var submitButton = document.getElementById('submit');
-				var passRegex = /\W?\w+/;
-				if(!passRegex.test(pass1.value)) {
-					pass2.style.backgroundColor = badColor;
-					message.style.color = badColor;
-					message.innerHTML = "Your password does not match the requirements.";
-					submitButton.type = "hidden";
-				}			
-				else if((pass1.value == pass2.value)){
-					//The passwords match.
-					pass2.style.backgroundColor = goodColor;
-					message.style.color = goodColor;
-					message.innerHTML = "Passwords Match!";
-					submitButton.type = "submit"
-				}
-				else{
-					//The passwords do not match.
-					pass2.style.backgroundColor = badColor;
-					message.style.color = badColor;
-					message.innerHTML = "Passwords Do Not Match!";
-					submitButton.type = "hidden";
-				}
-			} 
-		</SCRIPT>
-		<!--This creates the insert boxes-->
-		<form action="insertUser.php" method="post">
-			<input type="text" name="name" placeholder="Name" required /> <br>
-			<div class="fieldWrapper">
-			    <input type= "text" name="email" id="email" placeholder="Email" onkeyup="checkEmail(); return false;" type="text" required>
-			    <span id="emailConfirm" class="confirmMessage"></span>
-			</div>
-			<input type="text" name="uid" placeholder="Middlebury College ID" required /> <br>
-		    	<input type="password" name="pw" id="pass1" placeholder="Password" required> <br>
-			<div class="fieldWrapper">
-			    <input type="password" name="pass2" id="pass2" placeholder="Confirm Password" onkeyup="checkPass(); return false;" type="password" required>
-			    <span id="passwordConfirm" class="confirmMessage"></span>
-			</div>
-			<input type="hidden" id="submit" value="Create User!"/>	
-			<br> <br>
-		</form>
-	</div>
-</body>
+                                                position:absolute;                                
+                                background-color:gray;
+                                opacity: .9;
+                                width:100%;
+                                height: 2%;
+                                padding-top: 1%;
+                                padding-bottom: 1%;
+                                padding-right: 10%;
+                                padding-left: 10%;
+                                //border-width: 3%;
+                                //border-color: black;
+                                margin-top: 10%;
+                                margin-right: 0%;
+                                margin-left: -1%;
+                                z-index: 1;
+                        }
+                        .headerText {
+                                float: left;
+                                color: white;
+                                width: 13%  
+                        }
+                        .center {
+                                color:black;
+                                text-align:center;
+                        }
+                        .main {
+                                position: fixed;
+                                background-color: white;
+                                opacity: .9;
+                                width: 60%;
+                                height: 50%;
+                                overflow-y: scroll;
+                                padding-top: 5%;
+                                padding-bottom: 5%;
+                                padding-right: 10%;
+                                padding-left: 10%;
+                                border-width: 3%;
+                                border-color: black;
+                                border-radius: 25px;
+                                margin-top: 15%;
+                                margin-bottom: 5%;
+                                margin-right: 10%;
+                                margin-left: 10%;
+                                z-index = -1;
+                        }
+                        p {
+                                font-family:"Times New Roman";
+                                font-size:150%;
+                        }
+                        a:link {
+                                font-size: 1em;
+                                font-weight: normal;
+                                color: white;  
+                                text-decoration:none;
+                                
+                        }
+                        a:visited {
+                                font-size: 1em;
+                                font-weight: normal;
+                                color: white;  
+                                text-decoration:none;
+                        }
+                        a:hover {
+                                font-size: 1.2em;
+                                font-weight: bold;
+                                color: white;  
+                                text-decoration:none;
+                        }
+                        a:active {
+                                font-size: 1.2em;
+                                font-weight: bold;
+                                color: white;  
+                                text-decoration:none;
+                        }
+                </style>
+        </head>
+        
+        <body background=1003background1.png>
+                <div class="logo">
+                        <img src=MiddleburyPanther.jpg width="100%" height="100%">                
+                </div>                
+                <div class="header">
+                        <?php
+                                define('DB_SERVER','panther.cs.middlebury.edu');
+                                define('DB_USERNAME','wschaaf');
+                                define('DB_PASSWORD','wschaaf');
+                                define('DB_DATABASE','wschaaf_Calendar');
+                                $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("Could not connect");
+                                if(isset($_SESSION['User'])){
+                                        echo "<div class=headerText>Welcome ".$_SESSION['User']."</div>";
+                                        echo "<div class=headerText><a href='./events.php'>Home</a></div>";
+                                        echo "<div class=headerText><a href='./CreateEvent.php'>Create Event</a></div>";
+                                        echo "<div class=headerText><a href='./CreateOrganization.php'>Create Organization</a></div>";
+                                        echo "<div class=headerText><a href='./addMembers.php'>Add Members to Org</a></div>";
+                                        $sql2="SELECT supervisor FROM Users WHERE uid = '$_SESSION[uid]'";
+                                          if (!mysqli_query($con,$sql2)) {
+                                                die('Error: ' . mysqli_error());
+                                          }
+                                          else {
+                                                 //execute the SQL query
+                                                $result2 = mysqli_query($con,$sql2);            
+                                          }    
+                                        $row = mysqli_fetch_array($result2);
+                                        if($row['supervisor']==1) {
+                                                echo "<div class=headerText><a href='./approveEvents.php'>Approve Events</a></div>";
+                                        }
+                                        echo "<div class=headerText><a href='./logout.php'>Logout</a></div>";
+                                }
+                                else { 
+                                        //if session user is not set, show link to log in and to create user
+                                        $_SESSION['User'] = null;
+                                        echo "<div class=headerText><a href='./events.php'>Home</a></div>";
+                                        echo "<div class=headerText><a href='./login.php'>Log In</a></div>";
+                                        echo "<div class=headerText><a href='./CreateUser.php'>Create User</a></div>";
+                                }
+                        ?>
+                </div>
 
+
+
+                <div class="main"> 
+                        <div id='content'>
+							<p>Create an Account:</p>
+							Please use a different password from your Middlebury account
+						</div>
+						<br> <br>
+						<div id='content1'>
+							<SCRIPT LANGUAGE="javascript">
+								var goodColor = "#66cc66";
+								var badColor = "#ff6666";
+								function checkEmail() {
+									var email = document.getElementById('email');
+									var message = document.getElementById('emailConfirm');
+									var emailRegex = /\S+@\S+\.\S+/;
+									if(emailRegex.test(email.value)) {
+										email.style.backgroundColor = "#ffffff";
+										message.style.color = goodColor;
+										message.innerHTML = "Valid";
+										submitButton.type = "submit";
+									}
+									else {
+										email.style.backgroundColor = badColor;
+										message.style.color = badColor;
+										message.innerHTML = "Not valid email!";
+										submitButton.type = "hidden";
+									}
+								}		
+								function checkPass() {
+									//Store the password field objects into variables ...
+									var pass1 = document.getElementById('pass1');
+									var pass2 = document.getElementById('pass2');
+									//Store the Confimation Message Object ...
+									var message = document.getElementById('passwordConfirm');
+									var submitButton = document.getElementById('submit');
+									var passRegex = /\W?\w+/;
+									if(!passRegex.test(pass1.value)) {
+										pass2.style.backgroundColor = badColor;
+										message.style.color = badColor;
+										message.innerHTML = "Your password does not match the requirements.";
+										submitButton.type = "hidden";
+									}			
+									else if((pass1.value == pass2.value)){
+										//The passwords match.
+										pass2.style.backgroundColor = goodColor;
+										message.style.color = goodColor;
+										message.innerHTML = "Passwords Match!";
+										submitButton.type = "submit"
+									}
+									else{
+										//The passwords do not match.
+										pass2.style.backgroundColor = badColor;
+										message.style.color = badColor;
+										message.innerHTML = "Passwords Do Not Match!";
+										submitButton.type = "hidden";
+									}
+								} 
+							</SCRIPT>
+							<!--This creates the insert boxes-->
+							<form action="insertUser.php" method="post">
+								<input type="text" name="name" placeholder="Name" required /> <br>
+								<div class="fieldWrapper">
+									<input type= "text" name="email" id="email" placeholder="Email" onkeyup="checkEmail(); return false;" type="text" required>
+									<span id="emailConfirm" class="confirmMessage"></span>
+								</div>
+								<input type="text" name="uid" placeholder="Middlebury College ID" required /> <br>
+									<input type="password" name="pw" id="pass1" placeholder="Password" required> <br>
+								<div class="fieldWrapper">
+									<input type="password" name="pass2" id="pass2" placeholder="Confirm Password" onkeyup="checkPass(); return false;" type="password" required>
+									<span id="passwordConfirm" class="confirmMessage"></span>
+								</div>
+								<input type="hidden" id="submit" value="Create User!"/>	
+								<br> <br>
+							</form>
+						</div>
+                </div>        
+        </body>
 </html>
-
-
