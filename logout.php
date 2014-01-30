9d459d6311ec94ec76756fdcf1b1af5f90b6f9f8
sebastianmatt <smatt@middlebury.edu>
2014-01-30 14:58:11 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -47,7 +47,6 @@
                                 background-color: white;
                                 opacity: .9;
                                 width: 60%;
-                                height: 50%;
                                 overflow-y: scroll;
                                 padding-top: 5%;
                                 padding-bottom: 5%;
@@ -64,7 +63,7 @@
                         }
                         p {
                                 font-family:"Times New Roman";
-                                font-size:150%;
+                                font-size:1em;
                         }
                         a:link {
                                 font-size: 1em;
@@ -100,49 +99,22 @@
                 </div>                
                 <div class="header">
                         <?php
+                        		//Destroys the session
+								session_destroy();
                                 define('DB_SERVER','panther.cs.middlebury.edu');
                                 define('DB_USERNAME','wschaaf');
                                 define('DB_PASSWORD','wschaaf');
                                 define('DB_DATABASE','wschaaf_Calendar');
-                                $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("Could not connect");
-                                if(isset($_SESSION['User'])){
-                                        echo "<div class=headerText>Welcome ".$_SESSION['User']."</div>";
-                                        echo "<div class=headerText><a href='./events.php'>Home</a></div>";
-                                        echo "<div class=headerText><a href='./CreateEvent.php'>Create Event</a></div>";
-                                        echo "<div class=headerText><a href='./CreateOrganization.php'>Create Organization</a></div>";
-                                        echo "<div class=headerText><a href='./addMembers.php'>Add Members to Org</a></div>";
-                                        $sql2="SELECT supervisor FROM Users WHERE uid = '$_SESSION[uid]'";
-                                          if (!mysqli_query($con,$sql2)) {
-                                                die('Error: ' . mysqli_error());
-                                          }
-                                          else {
-                                                 //execute the SQL query
-                                                $result2 = mysqli_query($con,$sql2);            
-                                          }    
-                                        $row = mysqli_fetch_array($result2);
-                                        if($row['supervisor']==1) {
-                                                echo "<div class=headerText><a href='./approveEvents.php'>Approve Events</a></div>";
-                                        }
-                                        echo "<div class=headerText><a href='./logout.php'>Logout</a></div>";
-                                }
-                                else { 
-                                        //if session user is not set, show link to log in and to create user
-                                        $_SESSION['User'] = null;
-                                        echo "<div class=headerText><a href='./events.php'>Home</a></div>";
-                                        echo "<div class=headerText><a href='./login.php'>Log In</a></div>";
-                                        echo "<div class=headerText><a href='./CreateUser.php'>Create User</a></div>";
-                                }
+								echo "<div class=headerText><a href='./events.php'>Home</a></div>";
+								echo "<div class=headerText><a href='./login.php'>Log In</a></div>";
+								echo "<div class=headerText><a href='./CreateUser.php'>Create User</a></div>";
                         ?>
                 </div>
 
 
 
                 <div class="main"> 
-                        <?php 
-							//Destroys the session
-							session_destroy();
-							echo "<p>Thank you for logging out.</p>";
-						?>
+                        <p>Thank you for logging out!</p>
                 </div>        
         </body>
 </html>
