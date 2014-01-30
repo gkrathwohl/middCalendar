79bb02e6d18097949322ea1cffd4c0fa5b2334fb
sebastianmatt <smatt@middlebury.edu>
2014-01-30 15:08:46 -0500

    Update correctEvent.php

--- correctEvent.php
+++ correctEvent.php
@@ -45,7 +45,6 @@
                                 background-color: white;
                                 opacity: .9;
                                 width: 60%;
-                                height: 40%;
                                 overflow-y: scroll;
                                 padding-top: 5%;
                                 padding-bottom: 5%;
@@ -62,7 +61,7 @@
                         }
                         p {
                                 font-family:"Times New Roman";
-                                font-size:150%;
+                                font-size:1em;
                         }
                         .header a:link {
                                 font-size: 1em;
@@ -89,30 +88,6 @@
                                 color: white;
                                 text-decoration:none;
                         }
-			a:link {
-                                font-size: 1em;
-                                font-weight: normal;
-                                color: black;
-                                text-decoration:none;
-                                
-                        }
-                        a:visited {
-                                font-size: 1em;
-                                font-weight: normal;
-                                color: black;
-                                text-decoration:none;
-                        }
-                        a:hover {
-                                font-size: 1em;
-                                font-weight: bold;
-                                color: black;
-                                text-decoration:underline;
-                        }
-                        a:active {
-                                font-size: 1em;
-                                font-weight: bold;
-                                color: black;
-                                text-decoration:underline;
                 </style>
         </head>
         
@@ -183,7 +158,6 @@
 				   echo "Thank you for updating " .$name .".</br>";
 				}
 			?>
-	<a href='./events'>Back to home</a>
                 </div>        
         </body>
 </html>
