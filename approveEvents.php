5d0ea363d09d484d35f25c206ef8b5b4b5a7f6a1
wschaaf@middlebury.edu <wschaaf@farr.cs.middlebury.edu>
2014-01-31 14:13:30 -0500

    approve stuff

--- approveEvents.php
+++ approveEvents.php
@@ -17,9 +17,10 @@
 				z-index: 2;
 				opacity: .6;
 			}
-			.header {				
+			.header {					
 
-						position:absolute;				
+
+			position:absolute;				
 				background-color:gray;
 				opacity: .9;
 				width:100%;
@@ -84,7 +85,7 @@
 			a:hover {
 				font-size: 1.2em;
 				font-weight: bold;
-				color: white;  
+				color: black;  
 				text-decoration:none;
 			}
 			a:active {
@@ -93,6 +94,7 @@
 				color: white;  
 				text-decoration:none;
 			}
+			
 		</style>
 	</head>
 	
@@ -141,65 +143,65 @@
 
 		<div class="main"> 
 			<?php 
-if(isset($_SESSION['User'])){
+				if(isset($_SESSION['User'])){
 
-//set up the connection to the database
-define('DB_SERVER','panther.cs.middlebury.edu');
-define('DB_USERNAME','wschaaf');
-define('DB_PASSWORD','wschaaf');
+				//set up the connection to the database
+				define('DB_SERVER','panther.cs.middlebury.edu');
+				define('DB_USERNAME','wschaaf');
+				define('DB_PASSWORD','wschaaf');
 
-define('DB_DATABASE','wschaaf_Calendar');
+				define('DB_DATABASE','wschaaf_Calendar');
 
-$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("Could not connect");
+				$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("Could not connect");
 
-$sql="SELECT supervisor FROM Users WHERE uid = ".$_SESSION[uid];
+				$sql="SELECT supervisor FROM Users WHERE uid = ".$_SESSION[uid];
 
-if (!mysqli_query($con,$sql))
-{
-  die('Error: ' . mysqli_error());
-}
-else
-{
- $result = mysqli_query($con,$sql);
- $row = mysqli_fetch_array($result);
+				if (!mysqli_query($con,$sql))
+				{
+				  die('Error: ' . mysqli_error());
+				}
+				else
+				{
+				 $result = mysqli_query($con,$sql);
+				 $row = mysqli_fetch_array($result);
 
-if($row['supervisor']==0){
-	echo "must be supervisor to approve events";
-}else{
-
-
-$sql="SELECT * FROM Events WHERE date >= CURDATE() AND approved = 0";
-
-if (!mysqli_query($con,$sql))
-{
-  die('Error: ' . mysqli_error());
-}
-else
-{
- //execute the SQL query
- $result = mysqli_query($con,$sql);
-}
-
-echo "</br>All unapproved events (where date >= curdate): </br>";
-if(count($result) == 0){
-echo "There are no unapproved events";
-}else{
-while ($row = mysqli_fetch_array($result)) {
- 
- //print result
- echo "<a href='./eventInfo.php?eid=".$row['eid']."'>".$row['name']."</a> <a href='./approve.php?eid=".$row['eid']."'>Approve</a> <br>";
-}
-}
+				if($row['supervisor']==0){
+					echo "must be supervisor to approve events";
+				}else{
 
 
+				$sql="SELECT * FROM Events WHERE date >= CURDATE() AND approved = 0";
+
+				if (!mysqli_query($con,$sql))
+				{
+				  die('Error: ' . mysqli_error());
+				}
+				else
+				{
+				 //execute the SQL query
+				 $result = mysqli_query($con,$sql);
+				}
+
+				echo "</br>All unapproved events (where date >= curdate): </br>";
+				if(count($result) == 0){
+				echo "There are no unapproved events";
+				}else{
+				while ($row = mysqli_fetch_array($result)) {
+				 
+				 //print result
+				 echo "<a href='./eventInfo.php?eid=".$row['eid']."'>".$row['name']."</a> <a href='./approve.php?eid=".$row['eid']."'>Approve</a> <br>";
+				}
+				}
 
 
-}
-}
-}
-else{
-	echo "YOU MUST BE LOGGED IN TO APPROVE EVENTS";
-}
+
+
+				}
+				}
+				}
+				else{
+					echo "YOU MUST BE LOGGED IN TO APPROVE EVENTS";
+				}
 
 
 ?>
